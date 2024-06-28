a75c1d3bd34aa403ab0198f71afbbe0ad90501db
Ismael Amorim <ismaelcunha_@outlook.com>
2024-06-27 22:36:48 -0300

    fifth commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dizimo = document.querySelector('#valor-dizimo');
 const valor_dizimo = document.querySelector('#dizimo');
 const valor_pacto = document.querySelector('#pacto');
+const valor_total = document.querySelector('#total');
 const botao = document.querySelector('#botao');
 const percentual_dizimo = 0.10;
 const percentual_pacto = 0.01;
@@ -8,20 +9,18 @@ const percentual_pacto = 0.01;
 botao.addEventListener('click', () => {
     valor_dizimo.innerHTML = resultado_dizimo();
     valor_pacto.innerHTML = resultado_pacto();
+    valor_total.innerHTML = resultado_total();
 })
 
 function resultado_dizimo() {
-    // Calcular o valor do dízimo
     var valorDizimo = dizimo.value * percentual_dizimo;
   
-    // Formatar o valor em reais
     var valorFormatado = valorDizimo.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   
-    // Retornar o valor formatado
     return valorFormatado;
-  }
+}
 
-function resultado_pacto(){
+function resultado_pacto() {
     var valorPacto =  dizimo.value * percentual_pacto;
 
     var pactoFormatado = valorPacto.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL'});
@@ -29,6 +28,13 @@ function resultado_pacto(){
     return pactoFormatado;
 }
 
-function formatarReal(valor) {
-    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-  }
+function resultado_total() {
+    var valorDizimo = parseFloat(resultado_dizimo().replace(/\D/g, '').replace(',', '.')) / 100;
+    var valorPacto = parseFloat(resultado_pacto().replace(/\D/g, '').replace(',', '.')) / 100;
+
+    var valorTotal = valorDizimo + valorPacto;
+
+    var totalFormatado = valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+    return totalFormatado;
+}
